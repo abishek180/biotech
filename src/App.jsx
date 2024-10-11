37a11a8a94a74d4e680bfee3a6c1541ee7ef9e3a
abishek180 <anilabishek15@gmail.com>
2024-10-11 12:30:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Scene from './Scene'
+import Overlay from './assets/Overlay/Overlay'
+
+function App() {
+
+  return (
+    <>
+     <Scene/>
+     <Overlay/>
+    </>
+  )
+}
+
+export default App
